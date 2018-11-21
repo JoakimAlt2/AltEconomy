8d864dc09eabf1fce13b03601b7f9a28181aba5c
JoakimAlt2 <mr.sorokin.danila@gmail.com>
2018-11-21 14:41:41 +0300

    test again

--- src/main/java/ru/altspace/Network.java
+++ src/main/java/ru/altspace/Network.java
@@ -2,13 +2,16 @@ package ru.altspace;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class Network {
 
     private Connection con;
     private AltEconomy altEconomy = new AltEconomy();
     private Utils util = new Utils();
+    private static HashMap<String, PreparedStatement> preparedStatements = new HashMap<>();
 
     private String url = altEconomy.getConfig().getString("url");
     private String user = altEconomy.getConfig().getString("user");
@@ -17,10 +20,11 @@ public class Network {
 
     public Connection getConnection() { return con; }
 
-    public void createConnection() {
+    public void createConnection() throws SQLException {
 
         try {
 
+            Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(url, user, password);
             util.getLogger().info("База данных подключена!");
 
@@ -30,6 +34,26 @@ public class Network {
             sqlEx.printStackTrace();
             util.getLogger().info("База данных не подключена!\n" + "Возможные причины:\n" + "-Неправильно введены данные(url: " + url + "; user: " + user + "password: " + password + "\n-Проблемы с интернет соединением");
 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        synchronized (this) {
+            if (con != null && !con.isClosed()) {
+                return;
+            }
+
+            preparedStatements.put("addBalance", con.prepareStatement(
+
+                    "UPDATE INTO `alteconomy` (player, type, value) VALUES (?,?,?)"));
+
+            preparedStatements.put("getBalance", con.prepareStatement(
+
+                    "SELECT `type` FROM `alteconomy` WHERE `player`=? AND `type`=?"));
+
+            preparedStatements.put("addPlayer", con.prepareStatement(
+
+                    "INSERT INTO `alteconomy` (player) VALUES (?);"));
         }
 
     }
